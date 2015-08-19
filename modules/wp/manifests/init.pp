c4ec1c1516e42390c158195766636793f69b1d64
root <root@puppet.c.augmented-form-103908.internal>
2015-08-19 08:45:11 +0000

    Wed Aug 19 08:45:11 UTC 2015 puppet script commit

--- modules/wp/manifests/init.pp
+++ modules/wp/manifests/init.pp
@@ -1,12 +1,18 @@
 class wp::config {
 
-        file { "/var/www/wp-config.php":
-                ensure   => present,
-                owner  => "www-data",
-                group  => "www-data",
-		mode     => 0644,
-                source   => "puppet:///modules/wp/wp-config.php",
-                recurse  => true,
+	$DB_NAME = 'wordpress'
+	$DB_USER = 'wordpress'
+	$DB_U_PW = '-VQgsw2gDv'
+	$HOST =	'mysql.kitchenseeker.com'
+
+	file { "/var/www/wp-config.php":
+        	ensure => file,
+        	owner  => "www-data",
+        	group  => "www-data",
+        	mode   => 0644,
+        	content => template("wp/wp-config_php.erb")
         }
+
+                #source   => "puppet:///modules/wp/wp-config.php",
 }
 
