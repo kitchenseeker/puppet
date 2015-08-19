792a998080aa89e4027498f365eae69a2c9cbd3f
root <root@puppet.c.augmented-form-103908.internal>
2015-08-19 12:00:01 +0000

    Wed Aug 19 12:00:01 UTC 2015 puppet script commit

--- modules/wp/manifests/init.pp
+++ modules/wp/manifests/init.pp
@@ -11,6 +11,19 @@ class wp::stable {
 	$DB_U_PW = '-VQgsw2gDv'
 	$HOST =	'mysql.kitchenseeker.com'
 
+        vcsrepo { '/var/www':
+                notify => service["apache2"],
+                ensure   => present,
+                excludes => "/var/www/wp-config.php",
+		force   => "true",
+                owner  => "www-data",
+                group  => "www-data",
+                provider => git,
+                source   => 'https://github.com/kitchenseeker/www.git',
+                revision => 'master',
+        }
+
+
 	file { "/var/www/wp-config.php":
         	notify => service["apache2"],
 		ensure => file,
@@ -18,6 +31,39 @@ class wp::stable {
         	group  => "www-data",
         	mode   => 0644,
         	content => template("wp/wp-config_php.erb"),
+        }
+}
+
+class wp::dev {
+
+        service { "apache2":
+        ensure  => "running",
+        enable  => "true",
+        }
+
+
+        $DB_NAME = 'wordpress'
+        $DB_USER = 'wordpress'
+        $DB_U_PW = '-VQgsw2gDv'
+        $HOST = 'localhost'
+
+        file { "/var/www/wp-config.php":
+                notify => service["apache2"],
+                ensure => file,
+                owner  => "www-data",
+                group  => "www-data",
+                mode   => 0644,
+                content => template("wp/wp-config_php.erb"),
+        }
+
+        vcsrepo { '/var/www':
+                notify => service["apache2"],
+                ensure   => present,
+                owner  => "www-data",
+                group  => "www-data",
+                provider => git,
+                source   => 'https://github.com/kitchenseeker/www.git',
+                revision => 'development',
         }
 
 }
