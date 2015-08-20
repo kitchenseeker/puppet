cb13dc1f03dfc6ec23568ebe02a629da56a6b560
root <root@puppet.c.augmented-form-103908.internal>
2015-08-20 09:00:01 +0000

    Thu Aug 20 09:00:01 UTC 2015 puppet script commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,5 +29,13 @@ macro-useradd { "gianluka":
         sshkey  => "AAAAB3NzaC1yc2EAAAABIwAAAQEAtcT2WmjM0D2u4gLwY3zG3QGnXrIRBO3/MyGuSStpnb54PSYxNU4Hh4xTYZWllXc/zBpxgG43uJqOPOZfRqeqXFETswlyixSkMELxaCBJeyZkqH3o3vXdSkSkw3CHl60PwqhXNwaraSPa8dpZMyWfiSw23fXqQ7cskCK5qGyB6KnvggK8aeOtknHSwqVTqI/ueSRSRhi0cnzheldNTRLbg8dzYUSVMNiMI5fPckYLgksKKkYi1imW5DrVxCr5+a+XqY2fEKdiTBLK6Vsyc7GpGxOEBg8QRUyzTlxRkZSmj4k8zj0pvVYI5wKK2shPDTboyU7SweFPl0q2wIeBEoLtHw=="
 }
 
+macro-useradd { "igorvolovichev":
+        pass    => "!",
+        name    => "Software Engineer",
+        groups  => ["sudo","adm","www-data"],
+        uid     => "2001",
+        sshkey  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDTUxPFVkURnhd04sVbiuU0B4zjDrTG0l3b6cGauJjJFvhN5rueCE0o5vZikg7RXOe/dqqhZB7H9JIYtVonVgoAJubTuWH6Anszdj5DZ+eTTUjn8gBh4xzrm5nVG4izgfOfWB3iBlQcFzJOnr/zTBSSzsSBhJnrPast0L0E2RDQ3/baDvmlEztAiy1mwV2pAbccdWjFkNuSWv5wEHYbILh/P5PS9HEupmarLCtqhJxaZgR3cLP2Mkzoc6bcxEtNh8g7rMsjTiymNrgrtKQsOgwePHpWFlmVQBA5prRYv8PwWZ8RHhm06ypLehQPO9NDNV4wREAZTdTcyc8QBtJNVOc7"
+
+}
 
 
